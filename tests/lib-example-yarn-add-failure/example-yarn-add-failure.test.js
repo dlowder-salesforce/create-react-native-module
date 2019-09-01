3645047c36f6f2e7167674e7841b303c90797697
Christopher J. Brody <chris@brody.consulting>
2019-09-01 15:02:34 -0400

    XXX TBD async command WIP

--- tests/lib-example-yarn-add-failure/example-yarn-add-failure.test.js
+++ tests/lib-example-yarn-add-failure/example-yarn-add-failure.test.js
@@ -8,10 +8,11 @@ test('create module with example, with `yarn add` failure', async () => {
   const ioInjected = {
     ...ioInject,
     execa: {
-      commandSync: (command, _) => {
+      command: (command, _) => {
         if (/yarn add/.test(command)) {
-          throw new Error('ENOPERM not permitted');
+          return Promise.reject(new Error('ENOPERM not permitted'));
         }
+        return Promise.resolve();
       }
     }
   };
