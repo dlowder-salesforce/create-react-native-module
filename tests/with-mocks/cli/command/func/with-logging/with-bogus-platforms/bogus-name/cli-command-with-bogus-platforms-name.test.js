89bcb11ec85e374c6ddb53d56edc7c6d6dd1320c
Christopher J. Brody <chris@brody.consulting>
2019-09-03 18:10:47 -0400

    test cli-command.js with bogus values of platforms

---
+++ tests/with-mocks/cli/command/func/with-logging/with-bogus-platforms/bogus-name/cli-command-with-bogus-platforms-name.test.js
@@ -0,0 +1,45 @@
+const func = require('../../../../../../../../lib/cli-command.js').func;
+
+// special compact mocks for this test:
+const mysnap = [];
+const mockpushit = x => mysnap.push(x);
+jest.mock('elapsed-time', () => ({
+  new: () => ({
+    start: () => ({
+      getValue: () => `12.345s`
+    })
+  })
+}));
+jest.mock('fs-extra', () => ({
+  outputFile: (outputFileName, theContent) => {
+    mockpushit({ outputFileName, theContent });
+    return Promise.resolve();
+  },
+  ensureDir: (dir) => {
+    mockpushit({ ensureDir: dir });
+    return Promise.resolve();
+  },
+}));
+
+// TBD hackish mock:
+global.console = {
+  info: (...args) => {
+    mockpushit({ info: [].concat(args) });
+  },
+  log: (...args) => {
+    mockpushit({ log: [].concat(args) });
+  },
+  warn: (...args) => {
+    mockpushit({ warn: [].concat(args) });
+  },
+};
+
+test(`create alice-bobbi module with logging, with platforms: 'bogus'`, async () => {
+  const args = ['alice-bobbi'];
+
+  const options = { platforms: 'bogus' };
+
+  func(args, null, options);
+
+  expect(mysnap).toMatchSnapshot();
+});
